896b37bb80d3c04713c694265b297bb7aae6203e
SimonMaracine <simonmaracine@gmail.com>
2018-12-28 23:58:11 +0200

    Removing 'Tests' folder because it's not part of the project.

--- Tests/Try.py
+++
@@ -1,8 +0,0 @@
-try:
-    open("file.txt", "r")
-except Exception:
-    print "Could not find file; creating a new one."
-    open("file.txt", "w+")
-finally:
-    data_file = open("file.txt", "r")
-    print data_file.read()
