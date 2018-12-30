da9108ec1117e43c34517059dc2752a8c81e4f56
Simon Teodor Mărăcine <42865234+SimonMaracine@users.noreply.github.com>
2018-12-30 16:32:27 +0200

    Moving 'Button' class in its own file.

--- engine/room.py
+++ engine/room.py
@@ -27,24 +27,3 @@ class MainMenu(Room):
             if button.pressed():
                 return i
         return 16
-
-
-class Button(object):
-    def __init__(self, text, (x, y), font, actual_text):
-        self.text = text
-        self.x = x
-        self.y = y
-        self.width = font.size(actual_text)[0]
-        self.height = font.size(actual_text)[1]
-
-    def show(self, surface):
-        pygame.draw.rect(surface, (255, 16, 16), (self.x, self.y, self.width, self.height))
-        surface.blit(self.text, (self.x, self.y))
-
-    def pressed(self):
-        mouse_pos = pygame.mouse.get_pos()
-        if self.x + self.width > mouse_pos[0] > self.x:
-            if self.y + self.height > mouse_pos[1] > self.y and pygame.mouse.get_pressed()[0]:
-                return True
-        else:
-            return False
